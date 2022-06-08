7e98b596de3f42b2d87de1e6de3c91c1941075d1
strdtyfrudu <78678140+strdtyfrudu@users.noreply.github.com>
2022-06-08 09:27:00 +0430

    Update pornolab.py

--- pornolab.py
+++ pornolab.py
@@ -6,8 +6,8 @@
 
 # Replace YOUR_USERNAME_HERE and YOUR_PASSWORD_HERE with your Pornolab username and password
 credentials = {
-    'login_username': '',
-    'login_password': '',
+    'login_username': 'Hasankkkkk',
+    'login_password': '3tHZTBVT6wrU2zN',
 }
 
 # Logging
